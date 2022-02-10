358cbd21c6721877044bb6ac58fc66f2f71a3e46
Sc8Lorenz0 <98454597+Sc8Lorenz0@users.noreply.github.com>
2022-02-10 11:41:14 +0100

    AreaCerchio

---
+++ Esercizi/AreaCerchio.py
@@ -0,0 +1,8 @@
+
+
+print ("Area del cerchio")
+raggio = float(input("Per favore inserisci il raggio: "))
+area = 3.14159*(raggio**2)
+area = int(area)
+print ("")
+print ("L'area è di", area, "cm")
